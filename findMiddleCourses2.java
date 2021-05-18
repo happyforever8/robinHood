a9d347bac5d4d8ea7fab8c1512f6857f59c85b61
happyforever8 <83077456+happyforever8@users.noreply.github.com>
2021-05-18 11:22:43 -0400

    Update findMiddleCourses2.java

--- findMiddleCourses2.java
+++ findMiddleCourses2.java
@@ -1,7 +1,12 @@
 /*
-Students may decide to take different "tracks" or sequences of courses in the Computer Science curriculum. There may be more than one track that includes the same course, but each student follows a single linear track from a "root" node to a "leaf" node. In the graph below, their path always moves left to right.
+Students may decide to take different "tracks" or sequences of courses in the Computer Science curriculum.
+There may be more than one track that includes the same course,
+but each student follows a single linear track from a "root" node to a "leaf" node.
+In the graph below, their path always moves left to right.
 
-Write a function that takes a list of (source, destination) pairs, and returns the name of all of the courses that the students could be taking when they are halfway through their track of courses.
+Write a function that takes a list of (source, destination) pairs,
+and returns the name of all of the courses that the students could be taking
+when they are halfway through their track of courses.
 
 Sample input:
 all_courses = [

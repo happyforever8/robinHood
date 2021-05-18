e41b66a44d6c6d258a75ef6b879efd6f31de8062
happyforever8 <83077456+happyforever8@users.noreply.github.com>
2021-05-18 11:25:00 -0400

    Update findMiddleCourses2.java

--- findMiddleCourses2.java
+++ findMiddleCourses2.java
@@ -1,34 +1,29 @@
 /*
-Students may decide to take different "tracks" or sequences of courses in the Computer Science curriculum.
-There may be more than one track that includes the same course,
-but each student follows a single linear track from a "root" node to a "leaf" node.
-In the graph below, their path always moves left to right.
+有多种track，返回所有path里的中间那门课
 
-Write a function that takes a list of (source, destination) pairs,
-and returns the name of all of the courses that the students could be taking
-when they are halfway through their track of courses.
-
-Sample input:
-all_courses = [
-    ["Logic", "COBOL"],
-    ["Data Structures", "Algorithms"],
-    ["Creative Writing", "Data Structures"],
-    ["Algorithms", "COBOL"],
-    ["Intro to Computer Science", "Data Structures"],
-    ["Logic", "Compilers"],
-    ["Data Structures", "Logic"],
-    ["Creative Writing", "System Administration"],
-    ["Databases", "System Administration"],
-    ["Creative Writing", "Databases"],
-    ["Intro to Computer Science", "Graphics"],
+Sample input 1:
+all_courses_1 = [
+    ["Logic", "COBOL"],
+    ["Data Structures", "Algorithms"],
+    ["Creative Writing", "Data Structures"],
+    ["Algorithms", "COBOL"],
+    ["Intro to Computer Science", "Data Structures"],
+    ["Logic", "Compilers"],
+    ["Data Structures", "Logic"],
+    ["Graphics", "Networking"],
+    ["Networking", "Algorithms"],
+    ["Creative Writing", "System Administration"],
+    ["Databases", "System Administration"],
+    ["Creative Writing", "Databases"],
+    ["Intro to Computer Science", "Graphics"],
 ]
 
-Sample output (in any order):
-          ["Data Structures", "Creative Writing", "Databases", "Intro to Computer Science"]
+Sample output 1 (in any order):
+          ["Data Structures", "Networking", "Creative Writing", "Databases"]
 
 All paths through the curriculum (midpoint *highlighted*):
 
-*Intro to C.S.* -> Graphics
+Intro to C.S. -> Graphics -> *Networking* -> Algorithms -> Cobol
 Intro to C.S. -> *Data Structures* -> Algorithms -> COBOL
 Intro to C.S. -> *Data Structures* -> Logic -> COBOL
 Intro to C.S. -> *Data Structures* -> Logic -> Compiler
@@ -40,29 +35,43 @@ Creative Writing -> *Data Structures* -> Logic -> Compilers
 
 Visual representation:
 
-                    ____________
-                    |          |
-                    | Graphics |
-               ---->|__________|
-               |                          ______________
-____________   |                          |            |
-|          |   |    ______________     -->| Algorithms |--\     _____________
-| Intro to |   |    |            |    /   |____________|   \    |           |
-| C.S.     |---+    | Data       |   /                      >-->| COBOL     |
-|__________|    \   | Structures |--+     ______________   /    |___________|
-                 >->|____________|   \    |            |  /
-____________    /                     \-->| Logic      |-+      _____________
-|          |   /    ______________        |____________|  \     |           |
-| Creative |  /     |            |                         \--->| Compilers |
-| Writing  |-+----->| Databases  |                              |___________|
-|__________|  \     |____________|-\     _________________________
-               \                    \    |                       |
-                \--------------------+-->| System Administration |
-                                         |_______________________|
+                    ____________    ______________
+                    |          |    |            |
+                    | Graphics |    | Networking |
+               ---->|__________|--->|____________|
+               |                       |      
+               |                       |      
+               |                       |  ______________
+____________   |                       |  |            |
+|          |   |    ______________     >->| Algorithms |--\     _____________
+| Intro to |   |    |            |    /   |____________|   \    |           |
+| C.S.     |---+    | Data       |   /                      >-->| COBOL     |
+|__________|    \   | Structures |--+     ______________   /    |___________|
+                 >->|____________|   \    |            |  /
+____________    /                     \-->| Logic      |-+      _____________
+|          |   /    ______________        |____________|  \     |           |
+| Creative |  /     |            |                         \--->| Compilers |
+| Writing  |-+----->| Databases  |                              |___________|
+|__________|  \     |____________|-\     _________________________
+               \                    \    |                       |
+                \--------------------+-->| System Administration |
+                                         |_______________________|
+
+Sample input 2:
+all_courses_2 = [
+    ["Course_3", "Course_7"],
+    ["Course_0", "Course_1"],
+    ["Course_1", "Course_2"],
+    ["Course_2", "Course_3"],
+    ["Course_3", "Course_4"],
+    ["Course_4", "Course_5"],
+    ["Course_5", "Course_6"],
+]
+Sample output 2 (in any order):
+["Course_2", "Course_3"]
 
 Complexity analysis variables:
 
 n: number of pairs in the input
 
 */
-

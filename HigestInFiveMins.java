44af7b06bed5b5efe94b6281c240bf5a0ecf2cbd
happyforever8 <83077456+happyforever8@users.noreply.github.com>
2021-05-18 19:34:31 -0400

    Create HigestInFiveMins.java

---
+++ HigestInFiveMins.java
@@ -0,0 +1,43 @@
+First part was previously asked, but I didnt get much time to get into the the follow up question. Suggestions/approach?
+
+Suppose we have an unsorted log file of accesses to web resources.
+Each log entry consists of an access time, the ID of the user making the access, and the resource ID.
+The access time is represented as seconds since 00:00:00, and all times are assumed to be in the same day.
+
+For example:
+logs1 = [
+["58523", "user_1", "resource_1"],
+["62314", "user_2", "resource_2"],
+["54001", "user_1", "resource_3"],
+["200", "user_6", "resource_5"],
+["215", "user_6", "resource_4"],
+["54060", "user_2", "resource_3"],
+["53760", "user_3", "resource_3"],
+["58522", "user_22", "resource_1"],
+["53651", "user_5", "resource_3"],
+["2", "user_6", "resource_1"],
+["100", "user_6", "resource_6"],
+["400", "user_7", "resource_2"],
+["100", "user_8", "resource_6"],
+["54359", "user_1", "resource_3"],
+]
+
+Example 2:
+logs2 = [
+["300", "user_1", "resource_3"],
+["599", "user_1", "resource_3"],
+["900", "user_1", "resource_3"],
+["1199", "user_1", "resource_3"],
+["1200", "user_1", "resource_3"],
+["1201", "user_1", "resource_3"],
+["1202", "user_1", "resource_3"]
+]
+
+Write a function that takes the logs and returns the resource with the highest number of
+accesses in any 5 minute window, together with how many accesses it saw.
+
+Expected Output:
+most_requested_resource(logs1) # => ('resource_3', 3)
+
+
+

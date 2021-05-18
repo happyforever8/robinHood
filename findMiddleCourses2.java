21e09f00dc655841eecbc1aa8950b2ae38943f72
happyforever8 <83077456+happyforever8@users.noreply.github.com>
2021-05-18 18:06:26 -0400

    Update findMiddleCourses2.java

--- findMiddleCourses2.java
+++ findMiddleCourses2.java
@@ -89,3 +89,138 @@ Complexity analysis variables:
 n: number of pairs in the input
 
 */
+
+
+class Solution {
+  public static void main(String[] args) {
+    ArrayList<String> strings = new ArrayList<String>();
+    strings.add("Hello, World!");
+    strings.add("Welcome to CoderPad.");
+    strings.add("This pad is running Java " + Runtime.version().feature());
+
+    for (String string : strings) {
+      System.out.println(string);
+    }
+    
+    
+    String[][] courses = {{"Logic", "COBOL"},
+    {"Data Structures", "Algorithms"},
+    {"Creative Writing", "Data Structures"},
+    {"Algorithms", "COBOL"},
+    {"Intro to Computer Science", "Data Structures"},
+    {"Logic", "Compilers"},
+    {"Data Structures", "Logic"},
+    {"Graphics", "Networking"},
+    {"Networking", "Algorithms"},
+    {"Creative Writing", "System Administration"},
+    {"Databases", "System Administration"},
+    {"Creative Writing", "Databases"},
+    {"Intro to Computer Science", "Graphics"}};
+    
+    
+      Map<String, Set<String>> graph = buildGraph(courses);
+      Map<String, Integer> degree = new HashMap<>();
+
+      List<String> zeroDegree = findZeroDegree(graph, degree);
+
+
+      List<List<String>> courseGraph = new ArrayList<>();
+
+      for (String next : zeroDegree){
+        List<String> path = new ArrayList<>();
+        path.add(next);
+        
+        backTrack(courseGraph, path, graph, zeroDegree, next, new HashSet<>());
+      } 
+
+
+      List<String> result = new ArrayList<>();
+
+      for (List<String> list : courseGraph){
+        String[] arr = list.toArray(new String[list.size()]);
+
+        for (String s : arr){
+          System.out.print("****" + s + "***");
+          
+ 
+        }
+        
+         System.out.println("===========");
+        if (arr.length % 2 == 0){
+
+          result.add(arr[arr.length / 2 - 1]);
+        } else {
+          result.add(arr[arr.length / 2]);
+        }
+
+
+      }          
+       for (String next : result){
+          System.out.println(next);
+       }
+  
+  }
+  
+  
+  public static void backTrack(List<List<String>> courseGraph, List<String> list, Map<String, Set<String>> graph,
+                        List<String>  zeroDegree, String str, Set<String> set){
+     
+      if (graph.get(str).size() == 0){
+        
+         courseGraph.add(new ArrayList<>(list));
+        return;
+      }
+      set.add(str);
+      for (String next : graph.get(str)){ 
+        if (set.add(next)){
+          list.add(next);
+          backTrack(courseGraph, list, graph, zeroDegree, next, set);
+          list.remove(list.size() - 1);
+        }
+      }
+      set.remove(str);
+  }
+  
+    public static Map<String, Set<String>> buildGraph(String[][] courses){
+      Map<String, Set<String>> graph = new HashMap<>();
+
+       for (int i = 0; i < courses.length; i++){
+        for (int j = 0; j < courses[i].length; j++){
+          graph.putIfAbsent(courses[i][j], new HashSet<>());
+        }
+      }
+
+      for (int i = 0; i < courses.length; i++){
+        for (int j = 1; j < courses[i].length; j++){
+          graph.get(courses[i][0]).add(courses[i][j]);
+        }
+      }
+    
+      return graph;
+
+    }
+
+    public static List<String> findZeroDegree(Map<String, Set<String>> graph, Map<String, Integer> degree){
+
+
+      List<String> list = new ArrayList<>();
+
+      for (String key : graph.keySet()){
+        degree.put(key, 0);
+      }
+
+      for (String key: graph.keySet()){
+        for (String next : graph.get(key)){
+          degree.put(next, degree.getOrDefault(next, 0) + 1);
+        }
+      }
+
+      for (String key : graph.keySet()){
+        if (degree.get(key) == 0){
+          list.add(key);
+        }
+      }
+      return list;
+    }
+}
+

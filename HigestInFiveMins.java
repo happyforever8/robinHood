c8b27b5d65369660f04507bd86b50bc3c643eb26
happyforever8 <83077456+happyforever8@users.noreply.github.com>
2021-05-18 20:32:42 -0400

    Update HigestInFiveMins.java

--- HigestInFiveMins.java
+++ HigestInFiveMins.java
@@ -1,6 +1,7 @@
 First part was previously asked, but I didnt get much time to get into the the follow up question. Suggestions/approach?
 
-Suppose we have an unsorted log file of accesses to web resources. Each log entry consists of an access time, the ID of the user making the access, and the resource ID.
+Suppose we have an unsorted log file of accesses to web resources. Each log entry consists of an access time, 
+the ID of the user making the access, and the resource ID.
 The access time is represented as seconds since 00:00:00, and all times are assumed to be in the same day.
 
 For example:
@@ -32,15 +33,19 @@ logs2 = [
 ["1202", "user_1", "resource_3"]
 ]
 
-Write a function that takes the logs and returns the resource with the highest number of accesses in any 5 minute window, together with how many accesses it saw.
+Write a function that takes the logs and returns the resource with the highest
+number of accesses in any 5 minute window, together with how many accesses it saw.
 
 Expected Output:
 most_requested_resource(logs1) # => ('resource_3', 3)
 
 Follow Up
-Write a function that takes the logs as input, builds the transition graph and returns it as an adjacency list with probabilities. Add START and END states.
+Write a function that takes the logs as input, builds the transition graph and 
+returns it as an adjacency list with probabilities. Add START and END states.
 
-Specifically, for each resource, we want to compute a list of every possible next step taken by any user, together with the corresponding probabilities. The list of resources should include START but not END, since by definition END is a terminal state.
+Specifically, for each resource, we want to compute a list of every possible 
+next step taken by any user, together with the corresponding probabilities.
+  The list of resources should include START but not END, since by definition END is a terminal state.
 
 Expected output for logs1:
 transition_graph(logs1) # =>
@@ -54,7 +59,11 @@ transition_graph(logs1) # =>
 'resource_6': {'END': 0.5, 'resource_5': 0.5}
 }
 
-For example, of 8 total users, 4 users have resource_3 as a first visit (user_1, user_2, user_3, user_5), 2 users have resource_1 as a first visit (user_6, user_22), 1 user has resource_2 as a first visit (user_7), and 1 user has resource_6 (user_8) so the possible next steps for START are resource_3 with probability 4/8, resource_1 with probability 2/8, and resource_2 and resource_6 with probability 1/8.
+For example, of 8 total users, 4 users have resource_3 as a first
+visit (user_1, user_2, user_3, user_5), 2 users have resource_1 as a first visit (user_6, user_22),
+1 user has resource_2 as a first visit (user_7), and 1 user has resource_6 (user_8) so the possible
+ next steps for START are resource_3 with probability 4/8, resource_1 with probability 2/8, 
+and resource_2 and resource_6 with probability 1/8.
 
 These are the resource paths per user for the first logs example, ordered by access time:
 {

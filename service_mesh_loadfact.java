6520dc6fd30d08d2249c0938cf70a5b43e07f04d
happyforever8 <83077456+happyforever8@users.noreply.github.com>
2021-05-24 11:09:23 -0400

    Update service_mesh_loadfact.java

--- service_mesh_loadfact.java
+++ service_mesh_loadfact.java
@@ -1,6 +1,8 @@
 新鲜罗兵侠店面，新题，用topological sort算一个service mesh的loadfactor
 
-给一堆service call relations和一个entrypoint，比如 [a:b,c, b:c] a意思就是a会call b和c，b还会call c，然后assume entrypoint a有一个request，这样a的load就是1，b也是1，c是2。就是每个service都会completely fanout它收到的requests，比如如果c要有downstream的话，c给他每个downstream的load就是2.
+给一堆service call relations和一个entrypoint，比如 [a:b,c, b:c] a意思就是a会call b和c，b还会call c，
+然后assume entrypoint a有一个request，这样a的load就是1，b也是1，c是2。就是每个service都会completely fanout它收到的requests，
+比如如果c要有downstream的话，c给他每个downstream的load就是2.
 
 用topological sort做，要注意除了entrypoint之外还有别的service可能没有indegree，这些service 的load要是0，不然漏掉的话graph traverse不完
 
